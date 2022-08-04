f2f843a352ada42ab03df07a1fa684a46047f97f
Ashutosh Rai <ashutosh.developer78@gmail.com>
2022-08-04 19:24:54 +0530

    Final Commit

---
+++ lib/data_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_app/models.dart';
+
+class DataService {
+  Future<WeatherResponse> getWeather(String city) async {
+    // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+    final queryParameters = {
+      'q': city,
+      'appid': '3b2914a31287cdcfae1c3c535870c542',
+      'units': 'imperial'
+    };
+
+    final uri = Uri.https(
+        'api.openweathermap.org', '/data/2.5/weather', queryParameters);
+
+    final response = await http.get(uri);
+
+    print(response.body);
+    final json = jsonDecode(response.body);
+    return WeatherResponse.fromJson(json);
+  }
+}
